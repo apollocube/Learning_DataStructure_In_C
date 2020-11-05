2b8fcbcf5cb5629e2d1f48a5edf0ce4d01f1897b
ollopa <55067757+apollocube@users.noreply.github.com>
2020-11-05 11:52:25 +0800

    Add files via upload

---
+++ 20201105只能打印单个数字的函数用递归构建一个函数打印一串整数Print_a_number.c
@@ -0,0 +1,16 @@
+﻿/*Print_a_number*/
+#include <stdio.h>
+#include "USF_CODES.h"
+int main()
+{
+    int n=1560819;
+    print_number(n);
+    return 0;
+}
+
+int print_number(int x)
+{
+    if(x>10)
+        print_number(x/10);
+	PRINT_INT(x%10);
+};
---
+++ 20201105实现一个递归函数.c
@@ -0,0 +1,13 @@
+﻿/*瀹炵幇涓€涓€掑綊鍑芥暟F(0)=0, F(X)=2F(X-1)+X^2;*/
+double func001(double x)
+{
+    if(x==0)
+    return 0;
+    return 2*func001(x-1)+x*x;
+}
+#include <stdio.h>
+int main()
+{
+    printf("%f",func001(1));
+    return 0;
+}
